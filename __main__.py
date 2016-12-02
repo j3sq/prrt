a78225f7503c813e4c0b83a74a7210c2fa99cd0d
bellonemauro <bellonemauro@gmail.com>
2016-12-02 13:03:35 +0100

    added time measure and a simple comment

--- __main__.py
+++ __main__.py
@@ -5,6 +5,8 @@ import numpy as np
 import prrt.helper as helper
 from prrt.planner import Planner
 import os
+import time
+
 
 # configurations
 rebuild_tables = False
@@ -33,7 +35,7 @@ if rebuild_tables or not os.path.isfile('./prrt.pkl'):
     ptgs.append(bwd_circular_ptg)
 
     planner = Planner(ptgs)
-    planner.load_world_map('./lot.png', 117.6, 68.3)
+    planner.load_world_map('./lot_caseStudy.png', 117.6, 68.3) # this should not be done here ! 
     helper.save_object(planner, './prrt.pkl')
 else:
     planner = helper.load_object('./prrt.pkl')  # type: Planner
@@ -44,7 +46,13 @@ planner._ptgs.pop()
 # Set initial pose and goal pose
 init_pose = PoseR2S1(80, 60, 0.0 * np.pi)
 goal_pose = PoseR2S1(55, 10, -3./4 * np.pi)
+
+print(" SOLVING ")
+start = time.time()
 planner.solve(init_pose, goal_pose)
+end = time.time()
+print("time elapsed in s = ")
+print(end - start)
 
 # Plot as configured
 if plot_tree:
